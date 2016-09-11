3d133ba8ca32f207657ba5e2eb85315f8a6aa795
airlangga.cahya@gmail.com <airlangga.cahya@gmail.com>
2016-09-11 17:30:22 +0000

    Initial 7.0.10 core, core-composer, gd, and imagick tag

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REPO="dockerkit"
+PROJECT=$(pwd | rev | cut -d / -f 1 | rev)
+
+declare -a PIPELINE
+
+while IFS= read -r -d $'\0' BUILD_DIR; do
+    COUNT=$(grep -o / <<< "$BUILD_DIR" | wc -l | xargs printf "%03d")
+    PIPELINE+=("$COUNT $BUILD_DIR")
+done < <(find . -type f -name "Dockerfile" -print0)
+
+printf '%s\0' "${PIPELINE[@]}" | sort -z | while IFS= read -r -d $'\0' SORTED; do
+    BUILD_DIR=$(echo "$SORTED" | cut -sd ' ' -f 2- | cut -sd / -f 2- | rev | cut -sd / -f 2- | rev)
+    TAG=$(echo "$BUILD_DIR" | tr / -)
+    NAME="$REPO/$PROJECT:$TAG"
+    echo "Processing $NAME"
+    pushd "$BUILD_DIR" > /dev/null
+    docker build . -t "$NAME" || exit 1
+    docker push "$NAME"
+    popd > /dev/null
+done
